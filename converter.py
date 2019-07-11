db5c8020c89b085abec2c969051f2aec1c3b02bd
ttpham2 <Gartheer@gmail.com>
2019-07-11 13:19:53 -0700

    added command line option to specify image name prefix

--- converter.py
+++ converter.py
@@ -2,10 +2,12 @@
 
 # converter.py - Converts filenames of images  in a directory such that they can be imported into Opentoonz as a single level.
 
-import os, re
+import os, re, sys
 
+if len(sys.argv) > 1:
+	image_name = sys.argv[1]
 files = os.listdir()
-dirName = os.path.basename(os.getcwd())
+directory_name = os.path.basename(os.getcwd())
 images = []
 
 # TO-DO: Check filenames for images and stick them in a list
@@ -13,10 +15,12 @@ number_regex = re.compile(r'(\d{1,4}).(png|jpg|tiff)')
 for filename in files:
 	mo = number_regex.search(filename)
 	if mo is not None:
-		newname = dirName + '.' + ('0' * (4 - len(mo.group(1)))) + mo.group(1) + '.' + mo.group(2)
+		if len(sys.argv) > 1:
+			newname = image_name + '.' + ('0' * (4 - len(mo.group(1)))) + mo.group(1) + '.' + mo.group(2)
+		else:
+			newname = directory_name + '.' + ('0' * (4 - len(mo.group(1)))) + mo.group(1) + '.' + mo.group(2)
 		print('renaming ' + filename + ' to ' + newname)
 		os.rename(filename, newname)
 		
 print("All found filenames converted")
-# TO-DO: Convert image filenames
 
