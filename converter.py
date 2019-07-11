86a90320ec1a7bcc9f7ff99ed1af60a4b53c1747
ttpham2 <Gartheer@gmail.com>
2019-07-11 12:43:27 -0700

    fixed regular expression

--- converter.py
+++ converter.py
@@ -9,13 +9,14 @@ dirName = os.path.basename(os.getcwd())
 images = []
 
 # TO-DO: Check filenames for images and stick them in a list
-number_regex = re.compile(r'(\d)+.[png|jpg|tiff]')
+number_regex = re.compile(r'(\d{1,4}).(png|jpg|tiff)')
 for filename in files:
 	mo = number_regex.search(filename)
-	if mo.group(0) is not '':
+	if mo is not None:
 		newname = dirName + '.' + ('0' * (4 - len(mo.group(1)))) + mo.group(1) + '.' + mo.group(2)
+		print('renaming ' + filename + ' to ' + newname)
 		os.rename(filename, newname)
-	
-
+		
+print("All found filenames converted")
 # TO-DO: Convert image filenames
 
